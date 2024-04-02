5b7b8c5ec44a37fedd28e770d27ccc910e99ecca
Emily Santos <eyssantos@gmail.com>
2024-04-02 21:41:47 +0100

    Add small waves using cnoise

--- src/script.js
+++ src/script.js
@@ -9,6 +9,7 @@ import waterFragmentShader from './shaders/water/fragment.glsl'
  */
 // Debug
 const gui = new GUI({ width: 340 })
+const debugObject = {}
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -20,7 +21,11 @@ const scene = new THREE.Scene()
  * Water
  */
 // Geometry
-const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128)
+const waterGeometry = new THREE.PlaneGeometry(2, 2, 512, 512)
+
+// Colour
+debugObject.depthColor = '#186691'
+debugObject.surfaceColor = '#9bd8ff'
 
 // Material
 const waterMaterial = new THREE.ShaderMaterial({
@@ -28,18 +33,50 @@ const waterMaterial = new THREE.ShaderMaterial({
     fragmentShader: waterFragmentShader,
     uniforms: {
         utime: { value: 0 },
+
         uBigWavesElevation: { value: 0.2 },
         uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) },
         uBigWavesSpeed: { value: 0.75 },
+
+        uSmallWavesElevation: { value: 0.15 },
+        uSmallWavesFrequency: { value: 3 },
+        uSmallWavesSpeed: { value: 0.2 },
+        uSmallWavesIterations: { value: 4 },
+
+        uDepthColor: { value: new THREE.Color(debugObject.depthColor) },
+        uSurfaceColor: { value: new THREE.Color(debugObject.surfaceColor) },
+        uColorOffset: { value: 0.8 },
+        uColorMultiplier: { value: 4 }
     }
 })
 
 
-// Debug
+// Debug UI
 gui.add(waterMaterial.uniforms.uBigWavesElevation, 'value').min(0).max(1).step(0.001).name('uBigWavesElevation')
 gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'x').min(0).max(10).step(0.001).name('uBigWavesFrequencyX')
 gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'y').min(0).max(10).step(0.001).name('uBigWavesFrequencyY')
 gui.add(waterMaterial.uniforms.uBigWavesSpeed, 'value').min(0).max(4).step(0.001).name('uBigWavesSpeed')
+
+gui.addColor(debugObject, 'depthColor')
+   .name('depthColor')
+   .onChange(() =>   
+    {
+        waterMaterial.uniforms.uDepthColor.value.set(debugObject.depthColor)
+    })
+gui.addColor(debugObject, 'surfaceColor')
+   .name('surfaceColor')
+   .onChange(() =>   
+    {
+        waterMaterial.uniforms.uSurfaceColor.value.set(debugObject.surfaceColor)
+    })
+
+gui.add(waterMaterial.uniforms.uColorOffset, 'value').min(0).max(1).step(0.001).name('uColorOffset')
+gui.add(waterMaterial.uniforms.uColorMultiplier, 'value').min(0).max(10).step(0.001).name('uColorMultiplier')
+
+gui.add(waterMaterial.uniforms.uSmallWavesElevation, 'value').min(0).max(1).step(0.001).name('uSmallWavesElevation')
+gui.add(waterMaterial.uniforms.uSmallWavesFrequency, 'value').min(0).max(30).step(0.001).name('uSmallWavesFrequency') 
+gui.add(waterMaterial.uniforms.uSmallWavesSpeed, 'value').min(0).max(4).step(0.001).name('uSmallWavesSpeed')
+gui.add(waterMaterial.uniforms.uSmallWavesIterations, 'value').min(0).max(8).step(1).name('uSmallWavesIterations')
 
 // Mesh
 const water = new THREE.Mesh(waterGeometry, waterMaterial)
--- src/shaders/water/fragment.glsl
+++ src/shaders/water/fragment.glsl
@@ -1,6 +1,16 @@
+uniform vec3 uDepthColor;
+uniform vec3 uSurfaceColor;
+uniform float uColorOffset;
+uniform float uColorMultiplier;
+
+varying float vElevation;
+
 void main(){
     // output colour in sRGB olour space
     #include <colorspace_fragment>
 
-    gl_FragColor = vec4(0.5, 0.8, 1.0, 1.0);
+    float mixStregth = vElevation * uColorMultiplier + uColorOffset;
+    vec3 color = mix(uDepthColor, uSurfaceColor, mixStregth);
+
+    gl_FragColor = vec4(color, 1.0);
 }
